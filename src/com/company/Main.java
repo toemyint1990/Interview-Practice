e7612c5181997ae1fac530b75e95c8d5cb896f15
toemyint <toemyintthuster@gmail.com>
2020-03-05 09:47:33 +0800

    Adding more test cases

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.Set;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
 
@@ -20,5 +22,72 @@ public class Main {
         return 0;
 
     }
+
+    public static void fizzbuzz (int n) {
+
+        for (int i = 1; i <= n; i++) {
+            if (i%15 == 0) {
+                System.out.println("Fizzbuzz");
+
+            }
+            else if (i%3 == 0) {
+                System.out.println("Fizz");
+
+            }
+            else if (i%5 == 0) {
+                System.out.println("Buzz");
+
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void fizzBuzzJava8 (int n){
+       IntStream.rangeClosed(1,n)
+               .mapToObj(i -> i % 3 == 0 ? ( i % 5 == 0 ? "Fizzbuzz" : "Fizz")
+                       : ( i % 5 == 0 ? "Buzz" : i))
+               .forEach(System.out::println);
+    }
+
+    public static List<String> sortDates(List<String> dates) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
+        List<String> dateList = dates.stream()
+                .sorted(Comparator.comparing(s-> LocalDate.parse(s, formatter)))
+                .collect(Collectors.toList());
+
+        return dateList;
+    }
+
+    public static List<Integer> findTwoElements (List<Integer> list) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        int count = 1;
+        for (Integer integer : list) {
+            if (map.keySet().contains(integer)) {
+                int value = map.get(integer);
+                map.replace(integer, value + 1);
+            }
+            else map.put(integer, count);
+        }
+        map.entrySet().forEach(es -> {
+            if(es.getValue() == 1) result.add(es.getKey());
+        });
+        List<Integer> sub = result.subList(0,2);
+        return  sub;
+    }
+
+    public static String reverseString (String s) {
+        char[] charArray = s.toCharArray();
+        String st = new String();
+        for (int i = charArray.length - 1 ; i >= 0 ; i--) {
+            st += charArray[i] + " ";
+
+        }
+        return st;
+    }
+
+
 
 }
--- src/com/company/MainTest.java
+++ src/com/company/MainTest.java
@@ -3,6 +3,10 @@ package com.company;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
@@ -16,5 +20,37 @@ class MainTest {
         int[] intArray = new int[] {1,3,6,4,1,2};
         int actual = Main.findMinimumPositiveNumberNotOccurInArray(intArray);
         assertEquals(5, actual);
+    }
+
+    @Test
+    void fizzbuzzNormalFor() {
+        Main.fizzbuzz(15);
+    }
+
+    @Test
+    void fizzbuzzJava8() {
+        Main.fizzBuzzJava8(15);
+    }
+
+    @Test
+    void sortDates() {
+        List<String> dateList = new ArrayList<>();
+        dateList.add("01 Mar 2017");
+        dateList.add("21 Feb 2017");
+        dateList.add("21 Feb 2016");
+        dateList.add("12 Jan 2016");
+        Main.sortDates(dateList);
+    }
+
+    @Test
+    void findTwoElements () {
+        List<Integer> in = new ArrayList<>();
+        in.addAll(Arrays.asList(new Integer[]{5,23,23,66,5,23,7,0}));
+        Main.findTwoElements(in);
+    }
+
+    @Test
+    void reverseString () {
+        Main.reverseString("ToeMyint");
     }
 }
